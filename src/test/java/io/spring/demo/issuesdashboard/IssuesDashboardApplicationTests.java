10cf845e6e70247ebfebd72e31549c02ba76eb2c
Brian Clozel <bclozel@pivotal.io>
2017-11-23 15:28:05 +0100

    Create Spring Boot 2.0 web application on Initializr
    To create a new Spring Boot Application:
    
    * Go to start.spring.io
    * Choose the latest 2.0.0 Milestone
    * Select the "Web" starter dependency

---
+++ src/test/java/io/spring/demo/issuesdashboard/IssuesDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.demo.issuesdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssuesDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
