7b3a2aff6e0b819e1cc263918404d8b2d81afa4b
Brian Clozel <bclozel@pivotal.io>
2017-11-23 15:28:05 +0100

    Create Spring Boot 2.3 web application
    To create a new Spring Boot Application:
    
    * Go to start.spring.io
    * Choose the latest 2.x release
    * Select the "Web" starter dependency

---
+++ src/main/java/io/spring/demo/issuesdashboard/IssuesDashboardApplication.java
@@ -0,0 +1,12 @@
+package io.spring.demo.issuesdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesDashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/io/spring/demo/issuesdashboard/IssuesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.demo.issuesdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IssuesDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
