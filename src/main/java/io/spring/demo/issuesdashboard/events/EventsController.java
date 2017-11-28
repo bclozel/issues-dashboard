9f6f8587dcef7d9a3a8b1a18e29be8069bc75de0
Brian Clozel <bclozel@pivotal.io>
2017-11-28 15:49:40 +0100

    Add dashboard HTML page
    It's now time to add the Thymeleaf starter and a few WebJars to our
    application and start adding HTML pages to it.
    
    We can also add custom error pages to replace the whitelabel error
    page.

--- src/main/java/io/spring/demo/issuesdashboard/events/EventsController.java
+++ src/main/java/io/spring/demo/issuesdashboard/events/EventsController.java
@@ -1,9 +1,14 @@
 package io.spring.demo.issuesdashboard.events;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import io.spring.demo.issuesdashboard.github.GithubClient;
 import io.spring.demo.issuesdashboard.github.RepositoryEvent;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +31,16 @@ public class EventsController {
 
 		GithubProject project = this.repository.findByRepoName(projectName);
 		return this.githubClient.fetchEvents(project.getOrgName(), project.getRepoName()).getBody();
+	}
+
+	@GetMapping("/")
+	public String dashboard(Model model) {
+		List<DashboardEntry> entries = StreamSupport
+				.stream(this.repository.findAll().spliterator(), true)
+				.map(p -> new DashboardEntry(p, githubClient.fetchEventsList(p.getOrgName(), p.getRepoName())))
+				.collect(Collectors.toList());
+		model.addAttribute("entries", entries);
+		return "dashboard";
 	}
 
 }
