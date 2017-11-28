ccc3f22a8e57bd15e95d8857d3b849ec592848a0
Brian Clozel <bclozel@pivotal.io>
2017-11-28 16:39:59 +0100

    Enable Caching with Caffeine
    Because our Dashboard page is likely to be used a lot, we don't
    want to send multiple API requests to GitHub for each page refresh.
    
    First, we need to add the Spring Boot Cache Starter and a cache
    implementation - here, Caffeine.
    We can now add a new method to our `GithubClient` and stick
    a `@Cacheable("events")` annotation on it.
    
    Refreshing the page won't count against our rate-limit if the
    cache contains data.

--- src/main/java/io/spring/demo/issuesdashboard/IssuesDashboardApplication.java
+++ src/main/java/io/spring/demo/issuesdashboard/IssuesDashboardApplication.java
@@ -3,9 +3,11 @@ package io.spring.demo.issuesdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GithubProperties.class)
+@EnableCaching
 public class IssuesDashboardApplication {
 
 	public static void main(String[] args) {
